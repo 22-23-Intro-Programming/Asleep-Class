133d16f3fbcecb3f10cc553285a3d34f58632f4e
EvanPham23 <112999264+EvanPham23@users.noreply.github.com>
2022-11-11 09:39:55 -0800

    Evan's Zoo

---
+++ ZooProject10:26.py
@@ -0,0 +1,21 @@
+class Zoo:
+
+    def __init__(self):
+        self.name = "Evan's Zoo and Aquarium"
+        self.animals = animals
+        self.habitats = habitat
+        self.tickPrice = $10
+        self.maxCap = 500
+
+        animals = ["Gorillas", "Orcas", "Elephants", "AWS Students"]
+        habitats =["Forest", "Oceans", "Savannah", "Annie Wright Schools"]
+
+    def getAnimals(self):
+        return self.animals
+    def getHabitats(self):
+        return self.habitats
+    def getTickets(self):
+        return self.tickPrice
+    
+        
+        
